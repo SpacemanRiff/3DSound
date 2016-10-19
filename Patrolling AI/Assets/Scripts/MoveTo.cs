e4ee43efc34f1969fbe7bf1b2b62b21abb0895b8
Scott Troutman <troutmansm@gmail.com>
2016-10-19 16:37:54 -0500

    things and stuff

--- Patrolling AI/Assets/Scripts/MoveTo.cs
+++ Patrolling AI/Assets/Scripts/MoveTo.cs
@@ -12,6 +12,7 @@ public class MoveTo : MonoBehaviour {
 	NavMeshAgent agent;
 
 	public int destPoint;
+	private int currPoint;
 	private float currTime = 0;
 	private float waitTime = 3f;
 	private Animator animator;
@@ -21,21 +22,26 @@ public class MoveTo : MonoBehaviour {
 		agent.autoBraking = true; //Make sure the agent slows down and stops when it reaches a navPoint.
 		agent.destination = navPoints [destPoint].position; //Go to the first position without waiting.
 		animator = GetComponent<Animator>();
+		animator.SetBool ("Moving", true);
+		currPoint = destPoint;
 	}
 
 	private void MoveToNextPoint(){
 		if (currTime == 0) {
 			currTime = Time.time; //Set the currTime to the time (in seconds) of how long the game has been going on.
+			animator.SetBool("Moving", false);
 		}
 		if((Time.time - currTime) >= waitTime){
 			agent.destination = navPoints [destPoint].position; //Move to next position.
-			destPoint = (destPoint + 1) % navPoints.Length; //Choose next position at random
+			animator.SetBool("Moving", true);
+			destPoint = (destPoint + 1) % navPoints.Length; //Choose next position in line.
 			currTime = 0;
+			currPoint = destPoint;
 		}
 	}
 
 	void Update(){
-		if (agent.remainingDistance <= 0.1f) {
+		if (agent.remainingDistance <= 1.0f) {
 			MoveToNextPoint ();
 		}
 	}
