fc6eccd2754101f55430181148d262376a5b9ee5
Scott Troutman <troutmansm@gmail.com>
2016-10-21 10:37:41 -0500

    footsteps

---
+++ Patrolling AI/Assets/Scripts/FootstepController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootstepController : MonoBehaviour {
+
+    AudioSource audio;
+    public AudioClip[] footsteps;
+    // Use this for initialization
+    void Start () {
+        audio = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void PlayFootstep(float volume)
+    {
+        audio.PlayOneShot(footsteps[(int)Mathf.Floor(Random.value*8)],1);
+    }
+}
